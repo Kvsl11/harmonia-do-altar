63318b969390db949aecf6deb37b4f7dcf961142
KAIO VINICIUS SILVA LOPES <kaio.vsl11@gmail.com>
2025-07-26 00:00:27 -0400

    Delete formatCurrency.js

--- formatCurrency.js
+++
@@ -1,6 +0,0 @@
-function formatCurrency(value) {
-    if (typeof value !== 'number' || isNaN(value)) return 'R$ 0,00';
-    return `R$ ${value.toFixed(2).replace('.', ',')}`;
-}
-
-module.exports = formatCurrency;
